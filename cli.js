501a9fe2189927629ab377e16d2697576c1429d3
Joakim Hedlund <contact@joakimhedlund.com>
2019-03-28 21:18:33 +0100

    Add shebang notation for cmd-shim to create screenoff.cmd properly

--- cli.js
+++ cli.js
@@ -1,4 +1,7 @@
+#!/usr/bin/env node
 
 const screenoff = require('.')
-
 screenoff()
+  .catch(err => {
+    console.error('Something went horribly wrong. Do you have Powershell installed?', err)
+  })
